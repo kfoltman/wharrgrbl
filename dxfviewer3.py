e2923d8389f9c4b383c0508fd0e2dca012b06c6d
Krzysztof Foltman <wdev@foltman.com>
2016-05-02 20:52:39 +0100

    More source reorganisation.

--- dxfviewer3.py
+++ dxfviewer3.py
@@ -9,6 +9,7 @@ import dxfgrabber
 
 from PyQt4 import QtCore, QtGui
 from sender.jobviewer import *
+from helpers.dxf import dxfToObjects
 from helpers.gui import MenuHelper
 from helpers.geom import *
 from helpers.flatitems import *
@@ -16,9 +17,8 @@ from helpers.flatitems import *
 class DXFViewer(PreviewBase):
     def __init__(self, drawing):
         PreviewBase.__init__(self)
-        self.drawing = drawing
+        self.objects = dxfToObjects(drawing)
         self.operations = []
-        self.generateObjects()
         self.updateCursor()
     def getPen(self, item, is_virtual):
         if is_virtual:
@@ -29,58 +29,6 @@ class DXFViewer(PreviewBase):
         if item.marked:
             return self.activeItemPen
         return self.drawingPen
-    def generateObjects(self):
-        self.objects = []
-        for i in self.drawing.entities.get_entities():
-            it = type(i)
-            if it is dxfgrabber.entities.Circle:
-                self.objects.append(DrawingCircle(QtCore.QPointF(i.center[0], i.center[1]), i.radius))
-            elif it is dxfgrabber.entities.Arc:
-                if i.endangle > i.startangle:
-                    self.objects.append(DrawingArc(QtCore.QPointF(i.center[0], i.center[1]), i.radius, i.endangle * math.pi / 180.0, (i.startangle - i.endangle) * math.pi / 180.0))
-                else:
-                    self.objects.append(DrawingArc(QtCore.QPointF(i.center[0], i.center[1]), i.radius, i.startangle * math.pi / 180.0, twopi + (i.endangle - i.startangle) * math.pi / 180.0))
-            elif it is dxfgrabber.entities.Line:
-                self.objects.append(DrawingLine(QtCore.QPointF(i.start[0], i.start[1]), QtCore.QPointF(i.end[0], i.end[1])))
-            elif it is dxfgrabber.entities.LWPolyline:
-                nodes = []
-                points = []
-                for p in range(len(i.points)):
-                    points.append(i.points[p])
-                if i.is_closed:
-                    points.append(points[0])
-                for p in range(len(points) - 1):
-                    if i.bulge[p]:
-                        #print i.bulge[p]
-                        p1 = i.points[p]
-                        p2 = i.points[(p + 1) % len(i.points)]
-                        dx, dy = p2[0] - p1[0], p2[1] - p1[1]
-                        theta = 4 * math.atan(i.bulge[p])
-                        dist = math.sqrt(dx ** 2 + dy ** 2)
-                        a = math.atan2(dy, dx)
-                        d = dist / 2.0
-                        r = abs(d / math.sin(theta / 2))
-                        c = d / math.tan(theta / 2)
-                        da = math.pi / 2.0
-                        xm0 = (p1[0] + p2[0]) / 2.0
-                        ym0 = (p1[1] + p2[1]) / 2.0
-                        xm = xm0 - c * math.sin(a)
-                        ym = ym0 + c * math.cos(a)
-                        sangle = math.atan2(p1[1] - ym, p1[0] - xm)
-                        span = theta
-                        nodes.append(DrawingArc(QtCore.QPointF(xm, ym), r, sangle, span))
-                        #self.drawArcImpl(xm, ym, 0, 0, r, sangle, theta * 360 / (2 * math.pi), self.drawingPath, self.drawingPen)
-                    else:
-                        if points[p] != points[p + 1]:
-                            nodes.append(DrawingLine(QtCore.QPointF(*points[p]), QtCore.QPointF(*points[p + 1])))
-                        #if p == 0:
-                        #    polyline.moveTo(points[p][0], points[p][1])
-                        #polyline.lineTo(points[p + 1][0], points[p + 1][1])
-                polyline = DrawingPolyline(nodes)
-                self.objects.append(polyline)
-            else:
-                print str(it)
-        
     def createPainters(self):
         self.initPainters()
         self.drawingPath = QtGui.QGraphicsScene()
@@ -173,7 +121,6 @@ class DXFMainWindow(QtGui.QMainWindow, MenuHelper):
         self.viewer.createPainters()
         self.viewer.repaint()
     def onOperationGenerate(self):
-        f = file("test.nc", "w")
         ops = ["G90 G17"]
         tool = defaultTool
         lastTool = None
@@ -194,6 +141,7 @@ class DXFMainWindow(QtGui.QMainWindow, MenuHelper):
                         opsc, last, lastz = o.tool.followContour([p.cut(start, end)], z if not is_tab else max(z, defaultZTab), last, lastz)
                         ops += opsc
         ops += lastTool.moveTo(qpxy(0, 0), lastTool.clearance, last, lastz)
+        f = file("test.nc", "w")
         for op in ops:
             print op
             f.write("%s\n" % op)
---
+++ helpers/dxf.py
@@ -0,0 +1,56 @@
+from helpers.flatitems import *
+import dxfgrabber
+
+def dxfToObjects(drawing):
+    objects = []
+    for i in drawing.entities.get_entities():
+        it = type(i)
+        if it is dxfgrabber.entities.Circle:
+            objects.append(DrawingCircle(QtCore.QPointF(i.center[0], i.center[1]), i.radius))
+        elif it is dxfgrabber.entities.Arc:
+            if i.endangle > i.startangle:
+                objects.append(DrawingArc(QtCore.QPointF(i.center[0], i.center[1]), i.radius, i.endangle * math.pi / 180.0, (i.startangle - i.endangle) * math.pi / 180.0))
+            else:
+                objects.append(DrawingArc(QtCore.QPointF(i.center[0], i.center[1]), i.radius, i.startangle * math.pi / 180.0, twopi + (i.endangle - i.startangle) * math.pi / 180.0))
+        elif it is dxfgrabber.entities.Line:
+            objects.append(DrawingLine(QtCore.QPointF(i.start[0], i.start[1]), QtCore.QPointF(i.end[0], i.end[1])))
+        elif it is dxfgrabber.entities.LWPolyline:
+            nodes = []
+            points = []
+            for p in range(len(i.points)):
+                points.append(i.points[p])
+            if i.is_closed:
+                points.append(points[0])
+            for p in range(len(points) - 1):
+                if i.bulge[p]:
+                    #print i.bulge[p]
+                    p1 = i.points[p]
+                    p2 = i.points[(p + 1) % len(i.points)]
+                    dx, dy = p2[0] - p1[0], p2[1] - p1[1]
+                    theta = 4 * math.atan(i.bulge[p])
+                    dist = math.sqrt(dx ** 2 + dy ** 2)
+                    a = math.atan2(dy, dx)
+                    d = dist / 2.0
+                    r = abs(d / math.sin(theta / 2))
+                    c = d / math.tan(theta / 2)
+                    da = math.pi / 2.0
+                    xm0 = (p1[0] + p2[0]) / 2.0
+                    ym0 = (p1[1] + p2[1]) / 2.0
+                    xm = xm0 - c * math.sin(a)
+                    ym = ym0 + c * math.cos(a)
+                    sangle = math.atan2(p1[1] - ym, p1[0] - xm)
+                    span = theta
+                    nodes.append(DrawingArc(QtCore.QPointF(xm, ym), r, sangle, span))
+                    #self.drawArcImpl(xm, ym, 0, 0, r, sangle, theta * 360 / (2 * math.pi), self.drawingPath, self.drawingPen)
+                else:
+                    if points[p] != points[p + 1]:
+                        nodes.append(DrawingLine(QtCore.QPointF(*points[p]), QtCore.QPointF(*points[p + 1])))
+                    #if p == 0:
+                    #    polyline.moveTo(points[p][0], points[p][1])
+                    #polyline.lineTo(points[p + 1][0], points[p + 1][1])
+            polyline = DrawingPolyline(nodes)
+            objects.append(polyline)
+        else:
+            print "Unknown DXF type:", str(it)
+    return objects
+    
