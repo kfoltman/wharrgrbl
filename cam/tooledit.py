8d609a60e1e2c527d083f6e7ab7ad14db1aff014
Krzysztof Foltman <wdev@foltman.com>
2017-01-15 23:50:40 +0000

    Make tool property dialog code reusable.

--- cam/tooledit.py
+++ cam/tooledit.py
@@ -6,7 +6,7 @@ import sys
 
 defaultTool = tool.CAMTool(diameter = 2.0, feed = 200.0, plunge = 100.0, depth = 0.3)
 
-class ToolEditDlg(QDialog):
+class ToolEditDlg(PropertyDialog):
     properties = [
         FloatEditableProperty("Diameter", "diameter", "%0.2f", min = 0),
         FloatEditableProperty("Depth of cut", "depth", "%0.2f", min = 0, allow_none = True),
@@ -16,26 +16,4 @@ class ToolEditDlg(QDialog):
         FloatEditableProperty("Clearance height", "clearance", "%0.1f", min = 0),
     ]
     def __init__(self, tool):
-        QDialog.__init__(self)
-        self.tool = tool
-        self.origValues = { prop.attribute: getattr(self.tool, prop.attribute) for prop in self.properties }
-        self.initUI()
-    def initUI(self):
-        def lineEdit():
-            return QLineEdit()
-        self.grid = PropertySheetWidget(self.properties)
-        self.grid.setObjects([self.tool])
-        self.setLayout(QVBoxLayout())
-        self.layout().addWidget(self.grid)
-        buttons = QHBoxLayout()
-        pb = QPushButton("Cancel")
-        pb.clicked.connect(self.reject)
-        buttons.addWidget(pb)
-        pb = QPushButton("&OK")
-        pb.setDefault(True)
-        pb.clicked.connect(self.accept)
-        buttons.addWidget(pb)
-        self.layout().addLayout(buttons)
-    def rollback(self):
-        for k, v in self.origValues.items():
-            setattr(self.tool, k, v)
+        PropertyDialog.__init__(self, tool, ToolEditDlg.properties)
